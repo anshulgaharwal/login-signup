23b8ea9873005fcc7d263857e8c42fdaeedf2a65
anshulgaharwal <anshulgaharwal@gmail.com>
2025-12-23 13:52:31 +0530

    added html

--- src/components/loginSignup/loginSignup.jsx
+++ src/components/loginSignup/loginSignup.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
-import './loginSignup.css'
+import './LoginSignup.css'
 
-const loginSignup = () => {
+import user_icon from '../assets/person.png'
+import email_icon from '../assets/email.png'
+import password_icon from '../assets/password.png'
+
+const LoginSignup = () => {
     return(
         <div>
+            <div className="container">
+                <div className="header">
+                    <div className="text">Auth Page</div>
+                    <div className="underLine"></div>
+                </div>
+                <div className="inputs">
+                    <div className="input">
+                        <img src="user_icon" alt="" />
+                        <input type="text" />
+                    </div>
+                    <div className="input">
+                        <img src="email_icon" alt="" />
+                        <input type="email" />
+                    </div>
+                    <div className="input">
+                        <img src="password_icon" alt="" />
+                        <input type="password" />
+                    </div>
+                </div>
 
+            </div>
         </div>
-    )
+    );
 }
 
-export default loginSignup
+export default LoginSignup
